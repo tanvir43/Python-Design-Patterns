9fa6abcff293322c3c4ee544db28a5d70d82d6f7
tanvir43 <raihncse43@gmail.com>
2020-04-21 10:39:34 +0600

    basic class

---
+++ basic_class.py
@@ -0,0 +1,32 @@
+import math
+
+class Circle:
+
+    def __init__(self, radius):
+        """
+        Object Initialization with "radius" parameter
+        """
+        self.radius = float(radius)
+
+    def get_area(self):
+        """
+        method to calculate area
+        """
+        return math.pi * self.radius ** 2
+
+    def get_perimeter(self):
+        """
+        method to calculate perimeter
+        """
+        return 2 * math.pi * self.radius
+
+# Creating object from "Circle" class
+circle = Circle("5.00")
+
+print(f"Circle radius {circle.radius}")
+
+#Call "get_area()" method from "circle" object
+print(f"Area of the circle is {circle.get_area()}")
+
+#Call "get_perimeter" method from "circle" object
+print(f"Perimeter of the circle is {circle.get_perimeter()}")
