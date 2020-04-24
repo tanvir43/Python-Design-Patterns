a81f19c5a730ec768b90e8719c7267b6b0e45b32
tanvir43 <raihncse43@gmail.com>
2020-04-24 11:01:47 +0600

    second example accessor and mutator class method

---
+++ accessor_mutator2.py
@@ -0,0 +1,23 @@
+import math
+ 
+class Circle:
+ 
+    def __init__(self, radius):
+        self.__radius = radius #
+ 
+    def get_area(self):
+        return math.pi * self.__radius ** 2
+ 
+    def get_perimeter(self):
+        return 2 * math.pi * self.__radius
+ 
+    # accessor or getter method for radius attribute
+    def get_radius(self):
+        return self.__radius
+ 
+    # mutator or setter method for radius attribute
+    def set_radius(self, radius):
+        if not isinstance(radius, int):
+            print("Error: ", radius, "is not an int")
+            return
+        self.__radius = radius
